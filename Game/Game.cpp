ec2c4ebfe63f52e797acc7125dc0b72675a74e5e
cgalo <cgalo13@gmail.com>
2020-10-24 04:04:52 -0400

    Updated Board and Game classes
    Overview:
    - Removed old commented code, added and updated comments through the files
    - Declared and initiallized the method getMoves in the Board class

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -6,18 +6,20 @@
 
 Game::Game()
 {
-    p1 = new Player(white);
-    p2 = new Player(black);
-    masterBoard = new Board();
-    Space* p1Init = masterBoard->getValue(3,3);
-    masterBoard->setMove(0,0,10,white);
-    Space* p2Init = masterBoard->getValue(3,3);
-    masterBoard->setMove(3,3,10,black);
+    p1 = new Player(white);                     // Create the player 1
+    p2 = new Player(black);                     // Create the player 2
+    masterBoard = new Board();                  // Build the board for this game with all values to 0
+    masterBoard->setMove(0, 0, 10, white);      // Set the player 1 in the 0,0 index with 10 stones
+    masterBoard->setMove(3, 3, 10, black);      // Set player 2 in the end-right most index of the board with 10 stones
 
 }
 
 Game::~Game()
 {
+    /**
+     * Destructor for garbage collection of the Game object
+    */
+
     delete p1;
     delete p2;
     delete masterBoard;
@@ -25,6 +27,5 @@ Game::~Game()
 
 void Game::start()
 {
-    //std::cout << masterBoard->getValue(0,0)->getValue() << std::endl;
     masterBoard->printBoard();
 }
