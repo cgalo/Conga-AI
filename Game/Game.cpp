14320f05cf3ae02441990dc43e9fd8e85eb20c5e
cgalo <cgalo13@gmail.com>
2020-10-27 05:06:30 -0400

    Update Game.cpp
    Overview:
    - Refactor the code in the constructor to setup the initial board and players

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -7,8 +7,16 @@
 Game::Game()
 {
     masterBoard = new Board();                              // Build the board for this game with all values to 0
-    masterBoard->setMove(0, 0, 10, white);                  // Set the player 1 in the 0,0 index with 10 stones
-    masterBoard->setMove(3, 3, 10, black);                  // Set player 2 in the end-right most index of the board with 10 stones
+    // Now we set the players and their rocks in their respective positions of the board
+    const int initVal = 10;                                 // Each player starts with 10 rocks in their space
+    // Setup player white/1
+    Space * p1Space = masterBoard->getValue(0, 0);          // We get the 0,0 corner for white/player 1
+    p1Space->setPlayer(white);                              // Set the player 1 as owner of this space
+    p1Space->setValue(initVal);                             // Set the rocks for the player
+    // Now we set up the player black/2
+    Space* p2Space = masterBoard->getValue(3, 3);           // Get the right-end most corner for p2/black
+    p2Space->setPlayer(black);                              // Set the black player as owner of this space
+    p2Space->setValue(initVal);                             // Set the initial rocks for the player
 
 }
 
