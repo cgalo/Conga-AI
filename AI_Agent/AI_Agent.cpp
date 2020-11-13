727d86291f043de567dd29b2439d3a47214956ba
cgalo <cgalo13@gmail.com>
2020-11-12 23:46:12 -0500

    Created AI_Agent class

---
+++ AI_Agent/AI_Agent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Carlos Galo on 11/12/20.
+//
+
+#include "AI_Agent.h"
---
+++ AI_Agent/AI_Agent.h
@@ -0,0 +1,14 @@
+//
+// Created by Carlos Galo on 11/12/20.
+//
+
+#ifndef CONGA_AI_AI_AGENT_H
+#define CONGA_AI_AI_AGENT_H
+
+
+class AI_Agent {
+
+};
+
+
+#endif //CONGA_AI_AI_AGENT_H
