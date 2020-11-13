0ebf492a023968c34d28d8a21dc0068f333b4b5d
cgalo <cgalo13@gmail.com>
2020-11-12 23:55:16 -0500

    Update AI_Agent.h
    Declared the basic functions that will be needed to implement the AI

--- AI_Agent/AI_Agent.h
+++ AI_Agent/AI_Agent.h
@@ -5,9 +5,18 @@
 #ifndef CONGA_AI_AI_AGENT_H
 #define CONGA_AI_AI_AGENT_H
 
+#include <vector>
+#include "../Board/Board.h"
 
 class AI_Agent {
+private:
+    int maxP, minP;                                     // Who is the player we are maximizing and minimizing
 
+    // Methods
+    int MiniMax(Board* board, int depth, bool isMax);   // Recurse to return the best value/option for the given board
+public:
+    AI_Agent(int maxP, int minP);                       // Constructor that asks for the value of min & max player
+    std::vector<Space*> getBestMove(Board* board);      // Get the best move given the current state of the board
 };
 
 
